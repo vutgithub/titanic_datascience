f8a887948cd10260bfe28264206ada3a4eacb0a9
Thanh Vu <tvu@MacBook-Pro-de-Thanh.local>
2020-03-16 09:10:04 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+    packages=['titanic'],    packages=['titanic'],        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='titanic',
+    version='0.1',
+    description='Analysis of the Titanic dataset',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    # Substitute <github_account> with the name of your GitHub account
+    url='https://github.com/vutgithub/titanic_datascience',
+    author='Thanh Vu',  # Substitute your name
+    author_email='tvu@xebia.fr',  # Substitute your email
+    license='Publicis Sapient Engineering',
+    packages=['titanic'],
+    install_requires=[
+        'pypandoc>=1.4'
+    ],
+)
